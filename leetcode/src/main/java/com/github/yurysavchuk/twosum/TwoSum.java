6fa0a1ca25536491d7888d4edc8a6ccef0cf4d17
yurysavchuk <yury.savchuk95@gmail.com>
2021-08-05 20:38:53 +0200

    Two Sum issue

--- leetcode/src/main/java/com/github/yurysavchuk/twosum/TwoSum.java
+++ leetcode/src/main/java/com/github/yurysavchuk/twosum/TwoSum.java
@@ -3,8 +3,8 @@ package com.github.yurysavchuk.twosum;
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int [] result = new int[2];
-        for (int i = 0; i <= nums.length; i++) {
-            for (int j = i; j <= nums.length; j++) {
+        for (int i = 0; i <= nums.length - 2; i++) {
+            for (int j = i + 1; j <= nums.length - 1; j++) {
                 if (target == nums[i] + nums[j]) {
                     return new int[] {i, j};
                 }
--- leetcode/src/test/java/com/github/yurysavchuk/twosum/TwoSumTest.java
+++ leetcode/src/test/java/com/github/yurysavchuk/twosum/TwoSumTest.java
@@ -20,4 +20,34 @@ public class TwoSumTest {
         assertEquals(0, result[0]);
         assertEquals(1, result[1]);
     }
+
+    @Test
+    void twoSumSecond() {
+        // given:
+        TwoSum twoSum = new TwoSum();
+        int [] nums = { 3,2,4 };
+        int target = 6;
+
+        // when:
+        int [] result = twoSum.twoSum(nums, target);
+
+        // then:
+        assertEquals(1, result[0]);
+        assertEquals(2, result[1]);
+    }
+
+    @Test
+    void twoSumThird() {
+        // given:
+        TwoSum twoSum = new TwoSum();
+        int [] nums = { 3,3 };
+        int target = 6;
+
+        // when:
+        int [] result = twoSum.twoSum(nums, target);
+
+        // then:
+        assertEquals(0, result[0]);
+        assertEquals(1, result[1]);
+    }
 }
