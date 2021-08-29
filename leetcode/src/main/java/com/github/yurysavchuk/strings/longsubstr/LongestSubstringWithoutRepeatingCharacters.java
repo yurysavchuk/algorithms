99475c48077a3db6f6c15f5500f3dc3b8466844a
yurysavchuk <yury.savchuk95@gmail.com>
2021-08-29 22:10:12 +0200

    O^2 complexity

--- leetcode/src/main/java/com/github/yurysavchuk/strings/longsubstr/LongestSubstringWithoutRepeatingCharacters.java
+++ leetcode/src/main/java/com/github/yurysavchuk/strings/longsubstr/LongestSubstringWithoutRepeatingCharacters.java
@@ -2,31 +2,26 @@ package com.github.yurysavchuk.strings.longsubstr;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
-    public static Boolean areDistinct(String str, int i, int j) {
-        boolean[] visited = new boolean[26];
-
-        for(int k = i; k <= j; k++) {
-            if (visited[str.charAt(k) - 'a'] == true) {
-                return false;
-            }
-
-            visited[str.charAt(k) - 'a'] = true;
-        }
-        return true;
-    }
-
-
     public int lengthOfLongestSubstring(String str) {
         int n = str.length();
 
         int res = 0;
 
         for (int i = 0; i < n; i++) {
+            boolean[] visited = new boolean[256];
+
             for (int j = i; j < n; j++) {
-                if (areDistinct(str, i, j)) {
+
+                // if visited break the loop
+                if (visited[str.charAt(j)] == true) {
+                    break;
+                } else {
                     res = Math.max(res, j - i + 1);
+                    visited[str.charAt(j)] = true;
                 }
             }
+
+            visited[str.charAt(i)] = false;
         }
 
         return res;
--- leetcode/src/test/java/com/github/yurysavchuk/strings/longsubstr/LongestSubstringWithoutRepeatingCharactersTest.java
+++ leetcode/src/test/java/com/github/yurysavchuk/strings/longsubstr/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -18,4 +18,43 @@ public class LongestSubstringWithoutRepeatingCharactersTest {
         // then:
         assertEquals(3, result);
     }
+
+    @Test
+    void testCase2() {
+        // given:
+        LongestSubstringWithoutRepeatingCharacters sut = new LongestSubstringWithoutRepeatingCharacters();
+        String input = "bbbbb";
+
+        // when:
+        int result = sut.lengthOfLongestSubstring(input);
+
+        // then:
+        assertEquals(1, result);
+    }
+
+    @Test
+    void testCase3() {
+        // given:
+        LongestSubstringWithoutRepeatingCharacters sut = new LongestSubstringWithoutRepeatingCharacters();
+        String input = "pwwkew";
+
+        // when:
+        int result = sut.lengthOfLongestSubstring(input);
+
+        // then:
+        assertEquals(3, result);
+    }
+
+    @Test
+    void testCase4() {
+        // given:
+        LongestSubstringWithoutRepeatingCharacters sut = new LongestSubstringWithoutRepeatingCharacters();
+        String input = "";
+
+        // when:
+        int result = sut.lengthOfLongestSubstring(input);
+
+        // then:
+        assertEquals(0, result);
+    }
 }
