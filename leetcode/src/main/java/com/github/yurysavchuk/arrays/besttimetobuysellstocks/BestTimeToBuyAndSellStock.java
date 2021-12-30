c0fcb90138e495418390ec5e377c0ef92b925f00
yurysavchuk <yury.savchuk95@gmail.com>
2021-12-30 13:18:39 +0100

    Best Time to buy or sell stock - O(n) solution.

--- leetcode/src/main/java/com/github/yurysavchuk/arrays/besttimetobuysellstocks/BestTimeToBuyAndSellStock.java
+++ leetcode/src/main/java/com/github/yurysavchuk/arrays/besttimetobuysellstocks/BestTimeToBuyAndSellStock.java
@@ -3,29 +3,13 @@ package com.github.yurysavchuk.arrays.besttimetobuysellstocks;
 public class BestTimeToBuyAndSellStock {
 
     public int maxProfit(int[] prices) {
-        return calculate(prices, 0);
-    }
-
-    private int calculate(int[] prices, int index) {
-        if (index >= prices.length) {
-            return 0;
-        }
-        int max = 0;
-        for (int i = index; i < prices.length; i++) {
-            int maxProfit = 0;
-            for (int j = i  + 1; j < prices.length; j++) {
-                if (prices[i] < prices[j]) {
-                    int profit = calculate(prices, j + 1) + prices[j] - prices[i];
-                    if (profit > maxProfit) {
-                        maxProfit = profit;
-                    }
-                }
-            }
-            if (maxProfit > max) {
-                max = maxProfit;
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                maxProfit += prices[i] - prices[i - 1];
             }
         }
-        return max;
+        return maxProfit;
     }
 
 }
