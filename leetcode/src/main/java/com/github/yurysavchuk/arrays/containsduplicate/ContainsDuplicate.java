e31ba7f80221ed73fa56c68d6fdf5269638f1c40
yurysavchuk <yury.savchuk95@gmail.com>
2022-01-22 17:07:09 +0100

    Refactoring.

--- leetcode/src/main/java/com/github/yurysavchuk/arrays/containsduplicate/ContainsDuplicate.java
+++ leetcode/src/main/java/com/github/yurysavchuk/arrays/containsduplicate/ContainsDuplicate.java
@@ -8,11 +8,11 @@ public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> numSet = new HashSet<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (numSet.contains(nums[i])) {
+        for (int num : nums) {
+            if (numSet.contains(num)) {
                 return true;
             }
-            numSet.add(nums[i]);
+            numSet.add(num);
         }
         return false;
     }
