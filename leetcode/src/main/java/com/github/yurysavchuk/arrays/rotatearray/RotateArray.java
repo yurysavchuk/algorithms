21e745c052149c018638a75b6f209b0d3ae07858
yurysavchuk <yury.savchuk95@gmail.com>
2022-01-22 16:40:52 +0100

    Right rotation of the array.

--- leetcode/src/main/java/com/github/yurysavchuk/arrays/rotatearray/RotateArray.java
+++ leetcode/src/main/java/com/github/yurysavchuk/arrays/rotatearray/RotateArray.java
@@ -1,36 +1,20 @@
 package com.github.yurysavchuk.arrays.rotatearray;
 
 public class RotateArray {
-    // it's implementation of the left roation
-    // TODO implement the right rotation
-    public void rotate(int[] nums, int d) {
-        int n = nums.length;
-        d = d % n;
-        int gcd = gcd(d, n);
-
-        for (int i = 0; i < gcd; i++) {
-            int temp = nums[i];
-            int j = i;
-            while (true) {
-                int k = j + d;
-                if (k >= n) {
-                    k = k - n;
-                }
-                if (k == i) {
-                    break;
-                }
-                nums[j] = nums[k];
-                j = k;
-            }
-            nums[j] = temp;
-        }
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
     }
 
-    private int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        } else {
-            return gcd(b, a % b);
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
