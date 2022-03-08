0da2b6d727579e0ec99650f6a4890d9bb1d019e4
yurysavchuk <yury.savchuk95@gmail.com>
2022-03-08 19:44:22 +0100

    Solve Number of 1 Bits.

---
+++ leetcode/src/main/java/com/github/yurysavchuk/others/numbersonebit/NumbersOfOneBit.java
@@ -0,0 +1,16 @@
+package com.github.yurysavchuk.others.numbersonebit;
+
+public class NumbersOfOneBit {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int bits = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                bits++;
+            }
+            mask <<= 1;
+        }
+        return bits;
+    }
+}
